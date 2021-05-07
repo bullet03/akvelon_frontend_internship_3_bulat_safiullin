0b1bd3c093b6daa9327dbd0e95d69b9859e4758d
Bulat <bulatkazan@yahoo.com>
2021-05-07 17:29:22 +0300

    fix rename sliderPictures file

--- src/consts/sliderPictures.js
+++ src/consts/sliderPictures.js
@@ -1,4 +1,4 @@
-const picturesSlider = [
+const sliderPictures = [
   {picture: "https://apriori-center.org/wp-content/uploads/2016/07/Evolution_by_will_yen-500x500.png"},
   {picture: "https://i.pinimg.com/originals/64/62/24/6462243c87f1604b1d9046bac1bfe47f.jpg"},
   {picture: "https://bipbap.ru/wp-content/uploads/2018/06/img_resize.jpg"},
@@ -6,4 +6,4 @@ const picturesSlider = [
   {picture: "https://bipbap.ru/wp-content/uploads/2018/06/3ee998952c2016c72a9ad131db1ea00d.jpg"}
 ];
 
-export default picturesSlider;
+export default sliderPictures;
