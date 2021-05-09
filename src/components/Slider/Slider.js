8739f16c5f41da25bb13a6c6dbbfa895d6e07b73
Bulat <bulatkazan@yahoo.com>
2021-05-09 16:29:13 +0300

    fix Slider, change logic from map to single image output through index

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,9 +1,43 @@
+import { useState } from "react"
+import sliderImages from '../../consts/sliderImages.js'
+import styles from './Slider.module.css'
+
 const Slider = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const showNext = () => {
+    if (imageIndex === sliderImages.length - 1) {
+      return;
+    } else {
+      setImageIndex(imageIndex + 1);
+    }
+  }
+
+  const showPrev = () => {
+    if (imageIndex === 0) {
+      return;
+    } else {
+      setImageIndex(imageIndex - 1);
+    }
+  };
+
   return (
-    <>
-      Slider
-    </>
-  )
+    <section className={styles.container}>
+      <div className={styles.slider}>
+        <button type="button" onClick={showPrev} className={styles.leftArrow}>
+          {"<"}
+        </button>
+        <img
+          src={sliderImages[imageIndex]}
+          alt="picture"
+          className={styles.picture}
+        />
+        <button type="button" onClick={showNext} className={styles.rightArrow}>
+          {">"}
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Slider
