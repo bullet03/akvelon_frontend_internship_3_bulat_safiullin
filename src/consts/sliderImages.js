e49c71d49bcc6cb06bd510fee376a6f6670acfce
Bulat <bulatkazan@yahoo.com>
2021-05-09 16:25:37 +0300

    fix slideImages, changed name and structure from array of obj to array of strings

---
+++ src/consts/sliderImages.js
@@ -0,0 +1,9 @@
+const sliderImages = [
+  "https://apriori-center.org/wp-content/uploads/2016/07/Evolution_by_will_yen-500x500.png",
+  "https://i.pinimg.com/originals/64/62/24/6462243c87f1604b1d9046bac1bfe47f.jpg",
+  "https://bipbap.ru/wp-content/uploads/2018/06/img_resize.jpg",
+  "https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg",
+  "https://bipbap.ru/wp-content/uploads/2018/06/3ee998952c2016c72a9ad131db1ea00d.jpg"
+];
+
+export default sliderImages;
