b493e7da9abd66da5d6f3ce7e022b2705ce7ca30
Bulat <bulatkazan@yahoo.com>
2021-05-10 12:18:21 +0300

    fix CodingFucntions, removed fucntions to const

--- src/components/CodingFunctions/CodingFunctions.js
+++ src/components/CodingFunctions/CodingFunctions.js
@@ -1,56 +1,22 @@
 import styles from "./CodingFunctions.module.css";
-import factorial from '../../utils/factorial';
-import reverseBits from '../../utils/reverseBits';
-import uniqueWords from '../../utils/uniqueWords';
+import taskFunctions from "../../consts/taskFunctions"
 
 const CodingFunctions = () => {
+  const [factorial, reverseBits, uniqueWords] = taskFunctions;
 
   return (
     <section className={styles.container}>
       <div className={styles.functionCard}>
         <span className={styles.functionHead}>Factorial</span>
-        <div className={styles.functionCode}>
-          {`
-  const factorial = (num) => {
-      let result = 1;
-      for (let i = 1; i <= num; i += 1) {
-        result = result * i;
-      }
-      return result;
-  };
-
-            *can also use recursion
-          `}
-        </div>
+        <div className={styles.functionCode}>{factorial}</div>
       </div>
       <div className={styles.functionCard}>
         <span className={styles.functionHead}>ReverseBits</span>
-        <div className={styles.functionCode}>
-          {`
-    const reverseBits = (num) => {
-        const binary = num.toString(2);
-        const reverseBinary = binary.split("").reverse().join("");
-        const result = parseInt(reverseBinary, 2);
-        return result;
-    }
-
-    *can also use set and for
-          `}
-        </div>
+        <div className={styles.functionCode}>{reverseBits}</div>
       </div>
       <div className={styles.functionCard}>
         <span className={styles.functionHead}>uniqueWords</span>
-        <div className={styles.functionCode}>
-          {`
-  const uniqueWords = (str) => {
-      const arrFromStr = str.split(" ");
-      const result = arrFromStr.filter(
-        (word, index, array) => array.indexOf(word) === index
-      );
-      return result.join(" ");
-  };
-          `}
-        </div>
+        <div className={styles.functionCode}>{uniqueWords}</div>
       </div>
     </section>
   );
